781b54db42d30d64ce263d9a29994e3d4bba672f
TL <TL@gmail.com>
2021-05-23 12:42:44 +0530

    dev1 modified test1.java

--- DemoProj1/src/com/nt/test/Test1.java
+++ DemoProj1/src/com/nt/test/Test1.java
@@ -1,10 +1,17 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("XYZ");
-
+		Utility u=new Utility();
+		System.out.println(u.getSysDate());
+	}
+	
+	public void m1() {
+		System.out.println("Hai");
 	}
 
 }
