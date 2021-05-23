58c7c895e1c8be69613a8592980b6aa02512fcb3
TL <TL@gmail.com>
2021-05-23 12:05:23 +0530

    Utility class update

---
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -0,0 +1,15 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class Utility {
+
+	public String main(String s)
+	{
+		return s;
+	}
+	public Date getSysDate()
+	{
+		return new Date();
+	}
+}
